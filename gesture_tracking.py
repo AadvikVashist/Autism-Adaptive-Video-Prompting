6e3f01b846ef26d14b5e591c4ef3e29dd8874c10
Aadvik <vashist.aadvik@gmail.com>
2022-11-04 08:41:46 -0400

    pls

--- gesture_tracking.py
+++ gesture_tracking.py
@@ -6,11 +6,21 @@ import cv2
 import numpy as np
 import mediapipe as mp
 import tensorflow as tf
+import dlib
 from tensorflow.keras.models import load_model
 
 # initialize mediapipe
+
+
 mpHands = mp.solutions.hands
-hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+hands = mpHands.Hands(max_num_hands=2, min_detection_confidence=0.7)
+
+# mpFace = mp.solutions.face_detection
+# face_detection = mpFace.FaceDetection(model_selection=0, min_detection_confidence=0.5)
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+
 mpDraw = mp.solutions.drawing_utils
 
 # Load the gesture recognizer model
@@ -20,7 +30,7 @@ model = load_model('mp_hand_gesture')
 f = open('gesture.names', 'r')
 classNames = f.read().split('\n')
 f.close()
-print(classNames)
+# print(classNames)
 
 
 # Initialize the webcam
@@ -35,14 +45,28 @@ while True:
     # Flip the frame vertically
     frame = cv2.flip(frame, 1)
     framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Get hand landmark prediction
     result = hands.process(framergb)
-
+    faces = detector(gray)
     # print(result)
-    
-    className = ''
+    for face in faces:
+        x1 = face.left() # left point
+        y1 = face.top() # top point
+        x2 = face.right() # right point
+        y2 = face.bottom() # bottom point
 
+        # Look for the landmarks
+        landmarks = predictor(image=gray, box=face)
+        for n in range(0, 68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+
+            # Draw a circle
+            cv2.circle(img=frame, center=(x, y), radius=3, color=(0, 255, 0), thickness=-1)
+
+    className = ''
+    # frame.fill(0)
     # post process the result
     if result.multi_hand_landmarks:
         landmarks = []
@@ -58,15 +82,10 @@ while True:
             mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
 
             # Predict gesture
-            prediction = model.predict([landmarks])
-            # print(prediction)
-            classID = np.argmax(prediction)
-            className = classNames[classID]
-
-    # show the prediction on the frame
-    cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
-                   1, (0,0,255), 2, cv2.LINE_AA)
-
+            # prediction = model.predict([landmarks])
+            # # print(prediction)
+            # classID = np.argmax(prediction)
+            # className = classNames[classID]
     # Show the final output
     cv2.imshow("Output", frame) 
 
